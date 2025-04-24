0ac42a16d0ddd614ca18c2cb3f0970339462799e
Alex-Fence <bokuto@mail.ru>
2025-04-24 16:53:20 +0300

    start making copy function

--- FileSearcher.py
+++ FileSearcher.py
@@ -1,18 +1,24 @@
 import os
+import shutil
 import pathlib
 from datetime import datetime
 
+def copy_file_by_date(dest_path:str, file_name: str):
+    if os.path.exists(dest_path):
+        pass
+
 source_path = os.getcwd()
 print(source_path)
-date_dct: dict = dict()
+
 for current_dir, cur_dir_lst, files_lst in os.walk(source_path):
     for file_name in files_lst:
         file_path:str = os.path.join(current_dir, file_name)
         if  os.path.exists(file_path):            
             creation_date = os.path.getctime(file_path)
             creation_date = datetime.fromtimestamp(creation_date)
-            date_dct['year'] = creation_date.year
-            date_dct['month'] = creation_date.month
-            date_dct['day'] = creation_date.day
-            print(file_name, date_dct)
+            year = creation_date.year
+            month = creation_date.month
+            day = creation_date.day
+            print(file_name, year, month, day)
+        
         
